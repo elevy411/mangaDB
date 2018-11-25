9f72f225bcb2726b5239ce5b489e26364da9e078
Elliot <elevy411@uchicago.edu>
2018-11-25 00:24:10 -0500

    adding model and db

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,26 @@
 from app import app
 from flask import render_template
+from .forms import LoginForm
+
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {"username": 'Elliot'}
-    return  render_template('index.html',title="Home", user=user)
+    mangas = [
+        {
+            "title": "Naruto",
+            "author": "Masashi Kishimoto"
+        },
+        {
+            "title": "ZombiePowder",
+            "author": "Tite Kubo"
+        }
+    ]
+    return  render_template('index.html',title="Home", user=user,mangas=mangas)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html',title='Sign In', form=form)
