e9988e20aabc0fc962dcdc8d9156d122b3c0f1d1
Elliot <elevy411@uchicago.edu>
2018-11-25 02:14:44 -0500

    new table in db

--- app/models.py
+++ app/models.py
@@ -5,6 +5,19 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    manga = db.relationship('MangaOwned',backref='user',lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class MangaOwned(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(140),index=True,unique=True)
+    author = db.Column(db.String(140))
+    volume_owned = db.Column(db.String(140))
+    volume_available = db.Column(db.String(140))
+    finished_series= db.Column(db.String(140))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<{} - {}>'.format(self.title,self.author)
